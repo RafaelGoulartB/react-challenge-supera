b4662118d140014aa5165012a7dd2d22832afb50
RafaelGoulartB <rafagoulartb@gmail.com>
2021-03-12 09:14:25 -0300

    feat: add next-seo config (#1)

---
+++ config/seo.js
@@ -0,0 +1,12 @@
+const seoConfig = {
+  title: "React Challenge Supera",
+  description: "React Challenge Supera made with Next.js and Chakra UI",
+  openGraph: {
+    url: "https://github.com/RafaelGoulartB",
+    title: "React Challenge Supera",
+    description: "React Challenge Supera made with Next.js and Chakra UI",
+    site_name: "React Challenge Supera",
+  },
+};
+
+export default seoConfig;
